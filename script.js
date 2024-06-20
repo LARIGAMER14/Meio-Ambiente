099a556f013426ec4484b5e6ab11d1b46c22cd46
Larissa Czarnik <104375004+LARIGAMER14@users.noreply.github.com>
2024-06-20 08:35:37 -0300

    x

--- script.js
+++ script.js
@@ -9,38 +9,38 @@ const perguntas = [
         enunciado: "Assim que saiu da aula de dança você se depara com uma bateria de celular no chão, que foi descartada incorretamente. Qual é o seu primeiro pensamento?",
         alternativas: [
             {
-                texto: "Eu que não vou pegar, o lixo não é meu.",
-                afirmacao: "De começo você ficou procurando quem foi. "
+                texto: "Ignoro, o lixo não é meu.",
+                afirmacao: "Pode não ter se sentido diretamente responsável, mas é importante conscientizar sobre a responsabilidade coletiva. "
             },
             {
-                texto: "Vou procurar um lixo para isso.",
-                afirmacao: "Pelo menos assim eu jogo fora."
+                texto: "Decido pegar e jogar no lixo mais próximo.",
+                afirmacao: "Demonstrou preocupação com o ambiente e agiu para preservá-lo."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Logo após, você está comprando mantimentos no supermercado e percebe que as sacolas de plástico estão disponíveis gratuitamente. O que você faz?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Opta por levar suas próprias sacolas reutilizáveis",
+                afirmacao: "Está contribuindo ativamente para a redução da poluição plásticaEscolhe reduzir o consumo de plástico e contribuir para a preservação ambiental."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Pega as sacolas de plástico oferecidas para facilitar o transporte",
+                afirmacao: "Pode considerar formas alternativas de transporte dos produtos que não envolvam o uso de plástico descartável."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Saindo do mercado, você encontra uma mulher carregando suas compras com sacolas reutilizaveis, vendo isso você repensa sobre os impactos do lixo no meio ambiente, e decide mudar seus hábitos diários. Como você age?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Reduzo o consumo de plástico e começo a optar por produtos reutilizáveis.",
+                afirmacao: "Está contribuindo ativamente para a redução da poluição plástica."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Continuo com meus hábitos normais, pois acredito que pequenas mudanças não fazem diferença.",
+                afirmacao: "Pode reconsiderar a importância das pequenas ações individuais na preservação do meio ambiente."
             }
         ]
     },
@@ -58,15 +58,15 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você decide ir dar uma volta a praia para relaxar um pouco, mas chegando lá você encontra lixo deixado por outras pessoas. Como você lida com essa situação? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Vou recolher o lixo encontrado para descartá-lo corretamente em um ponto de coleta",
+                afirmacao: "Demonstra compromisso com a conservação dos ecossistemas e a preservação da vida selvagem."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Ignora o lixo, pois não foi você quem o deixou lá.",
+                afirmacao: "Pode considerar o impacto ambiental coletivo de deixar o lixo nas praias e refletir sobre a responsabilidade compartilhada na preservação ambiental. "
             }
         ]
     },
