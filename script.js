f09080167730e1c662404462fb9586088dd9e543
Larissa Czarnik <104375004+LARIGAMER14@users.noreply.github.com>
2024-06-20 09:37:41 -0300

    f

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Reduzo o consumo de plástico e começo a optar por produtos reutilizáveis.",
-                afirmacao: "ajudando na redução da poluição plástica, você inncentiva os outros e faz a diferença."
+                afirmacao: "ajudando na redução da poluição plástica, você incentiva os outros e faz a diferença."
             },
             {
                 texto: "Continuo com meus hábitos normais, pois acredito que pequenas mudanças não fazem diferença.",
